c9a817c5de889f50b84c08c7a7d9f745f41fc775
Ezmad-Ze <90828396+Ezmad-Ze@users.noreply.github.com>
2022-10-13 23:00:07 +0300

    Adding UseEffect on the App.js,Adding Loading Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import { useState } from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Cards from "./Components/Cards";
 import NavBar from "./Components/NavBar";
 import { ThemeContext } from "./Context/ThemeContext";
+import { CountryContext } from "./Context/CountryContext";
+import { BASE_API } from "./Components/APIs";
 
 function App() {
   const [theme, setTheme] = useState("dark");
-
+  const [country, setCountry] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const toggleTheme = () => {
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
 
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(BASE_API)
+      .then((res) => {
+        setCountry(
+          res.data.sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
+        );
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+    
+  }, []);
+
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div className="App" id={theme}>
-        <NavBar />
-        <Cards />
-      </div>
+      <CountryContext.Provider value={{ country, setCountry }}>
+        <div className="App" id={theme}>
+          <NavBar />
+          <Cards load={isLoading} />
+        </div>
+      </CountryContext.Provider>
     </ThemeContext.Provider>
   );
 }
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,49 +1,39 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { BASE_API } from "./APIs";
+import React, { useContext } from "react";
+import { CountryContext } from "../Context/CountryContext";
 
-const Cards = () => {
-  const [country, setCountry] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(BASE_API)
-      .then((res) => {
-        setCountry(
-          res.data.sort((a, b) => (a.name.common > b.name.common ? 1 : -1))
-        );
-      })
-      .catch((err) => console.log(err));
-  }, []);
+const Cards = ({ load }) => {
+  const { country } = useContext(CountryContext);
 
   const CountryCard = () => {
     return (
-      <main>
-        <div className="main--style">
-          {country.map((coun) => (
-            <div key={coun.name.official} className="card">
-              <img src={coun.flags.png} alt="" className="card--image" />
-              <h3 className="card--title mode">{coun.name.common}</h3>
-              <p className="card--info mode">
-                <span className="card--info__subtitle">Population:</span>{" "}
-                {coun.population.toLocaleString()}
-              </p>
-              <p className="card--info mode">
-                <span className="card--info__subtitle">Region:</span>{" "}
-                {coun.region}
-              </p>
-              <p className="card--info mode">
-                <span className="card--info__subtitle">Capital:</span>{" "}
-                {coun.capital}
-              </p>
-            </div>
-          ))}
-        </div>
-      </main>
+      <div className="main--style">
+        {country.map((coun) => (
+          <div key={coun.name.official} className="card">
+            <img src={coun.flags.png} alt="" className="card--image" />
+            <h3 className="card--title mode">{coun.name.common}</h3>
+            <p className="card--info mode">
+              <span className="card--info__subtitle">Population:</span>{" "}
+              {coun.population.toLocaleString()}
+            </p>
+            <p className="card--info mode">
+              <span className="card--info__subtitle">Region:</span>{" "}
+              {coun.region}
+            </p>
+            <p className="card--info mode">
+              <span className="card--info__subtitle">Capital:</span>{" "}
+              {coun.capital}
+            </p>
+          </div>
+        ))}
+      </div>
     );
   };
 
-  return <>{CountryCard()}</>;
+  return (
+    <main>
+      {load ? <div className="loading">Loading 👇 👇 👇</div> : CountryCard()}
+    </main>
+  );
 };
 
 export default Cards;
